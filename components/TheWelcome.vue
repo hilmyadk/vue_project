f05c4b0a020fea573181b55197dab2cd020a183b
hilmyadk <muhammadhilmyandika@student.uir.ac.id>
2024-04-27 19:26:59 +0700

    Add files via upload

---
+++ components/TheWelcome.vue
@@ -0,0 +1,36 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import BioIcon from './icons/IconDocumentation.vue'
+import SkillIcon from './icons/IconTooling.vue'
+import ExperienceIcon from './icons/IconEcosystem.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <BioIcon />
+    </template>
+    <template #heading>Biodata Diri</template>
+
+    Perkenalkan nama saya Muhammad Hilmy Andika, saya lahir di Pekanbaru, Riau. Saya merupakan mahasiswa Universitas Islam Riau angkatan 22 dari jurusan Teknik Informatika.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SkillIcon />
+    </template>
+    <template #heading>Skill & Kemampuan</template>
+
+    Dalam hal skill dan kemampuan sebenarnya saya belum memiliki kedua hal itu ketika saya pertama kali ngoding, namun seiring berjalannya waktu sampai sekarang saya sudah belajar banyak hal tentang codingan.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ExperienceIcon />
+    </template>
+    <template #heading>Pengalaman yang pernah dimiliki</template>
+
+    Saya memiliki pengalaman dalam bekerja sama secara tim yaitu ketika membuat tampilan UI suatu aplikasi atau website. <br>
+    Salah satu contohnya proyeknya yaitu membuat tampilan UI untuk aplikasi Mobile Banking.
+  </WelcomeItem>
+</template>
---
+++ views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h2>For More Information <br> 
+    Please contact <br>
+    Email : <br> muhammadhilmyandika@student.uir.ac.id <br>
+    Whatsapp : <br> 082379103726
+    </h2>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
